15c8bcb33179bc4ad2f7b2927726c5288ace3eb2
martinIvovIv <martinivoviv@gmail.com>
2024-06-15 23:05:26 +0200

    feat(pages-MVP): adds MVP mission page

--- src/app/about/mission/page.tsx
+++ src/app/about/mission/page.tsx
@@ -1,10 +1,37 @@
-// import { TextBox } from '@/components/ui/text-box';
-// import * as m from '@/paraglide/messages';
+import { Heading } from '@/components/ui/heading';
+import { TextBox } from '@/components/ui/text-box';
+import * as m from '@/paraglide/messages';
 
 const Mission = () => {
   return (
     <section className="container flex flex-col items-center gap-3 py-4 sm:py-5 md:py-7 lg:py-8">
-      PlaceHolder
+      <TextBox className="flex flex-col gap-4">
+        <Heading>{m.about_us_mission_about_heading_one()}</Heading>
+        <p>{m.about_us_mission_about_p_one()}</p>
+        <p>{m.about_us_mission_about_p_two()}</p>
+        <div className="flex flex-col gap-0.5">
+          <p>{m.about_us_mission_about_p_three()}</p>
+          <p>{m.about_us_mission_about_p_four()}</p>
+          <p>{m.about_us_mission_about_p_five()}</p>
+          <p>{m.about_us_mission_about_p_six()}</p>
+          <p>{m.about_us_mission_about_p_seven()}</p>
+        </div>
+      </TextBox>
+      <TextBox className="flex flex-col gap-4">
+        <Heading>{m.about_us_mission_about_heading_two()}</Heading>
+        <p>{m.about_us_mission_about_p_two_one()}</p>
+        <div className="flex flex-col gap-1">
+          <h3>{m.about_us_mission_about_ul_label()}</h3>
+          <ul className="list-inside list-disc">
+            <li>{m.about_us_mission_about_li_one()}</li>
+            <li>{m.about_us_mission_about_li_two()}</li>
+            <li>{m.about_us_mission_about_li_three()}</li>
+            <li>{m.about_us_mission_about_li_four()}</li>
+            <li>{m.about_us_mission_about_li_five()}</li>
+          </ul>
+        </div>
+        <p>{m.about_us_mission_about_two_two()}</p>
+      </TextBox>
     </section>
   );
 };
