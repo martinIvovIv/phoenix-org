288539abfef9c6701a1b17d08fa42e53e941e836
martinIvovIv <martinivoviv@gmail.com>
2024-06-15 17:46:17 +0200

    feat(pages-MVP): adds MVP junior page

--- src/app/junior/page.tsx
+++ src/app/junior/page.tsx
@@ -1,10 +1,26 @@
-// import { TextBox } from '@/components/ui/text-box';
-// import * as m from '@/paraglide/messages';
+import { TextBox } from '@/components/ui/text-box';
+import { Link } from '@/lib/i18n';
+import * as m from '@/paraglide/messages';
 
 const FenixJunior = () => {
   return (
     <section className="container flex flex-col items-center gap-3 py-10">
-      PlaceHolder
+      <TextBox>{m.fenix_jnr_p_one()}</TextBox>
+      <TextBox>{m.fenix_jnr_p_two()}</TextBox>
+      <TextBox>{m.fenix_jnr_p_three()}</TextBox>
+      <TextBox>{m.fenix_jnr_p_four()}</TextBox>
+      <TextBox>{m.fenix_jnr_p_five()}</TextBox>
+      <TextBox>
+        {m.fenix_jnr_p_six_one()}
+        <Link
+          className="underline underline-offset-1 hover:underline-offset-2"
+          href="https://www.facebook.com/fenixJunior"
+        >
+          {m.fenix_jnr_p_six_link()}
+        </Link>
+        {m.fenix_jnr_p_six_two()}
+      </TextBox>
+      <TextBox className="font-bold">{m.fenix_jnr_bold_seven()}</TextBox>
     </section>
   );
 };
