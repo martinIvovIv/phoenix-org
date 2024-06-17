bdb9429d37ad09549735d944d46c79c39bd58d0c
martinIvovIv <martinivoviv@gmail.com>
2024-06-17 20:35:51 +0200

    fix(pages-MVP): finishes MVP activities page

--- src/app/about/activities/page.tsx
+++ src/app/about/activities/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 import { Heading } from '@/components/ui/heading';
 import { TextBox } from '@/components/ui/text-box';
 import * as m from '@/paraglide/messages';
 
+// TODO: translate image alt
 const Activities = () => {
   return (
     <section className="container flex flex-col items-center gap-3 py-4 sm:py-5 md:py-7 lg:py-8">
@@ -13,8 +16,15 @@ const Activities = () => {
       <TextBox className="flex flex-col gap-3 md:gap-4">
         <Heading>{m.about_us_activities_heading_two()}</Heading>
         <p>{m.about_us_activities_p_two_one()}</p>
-
-        <p className="self-center">X image</p>
+      </TextBox>
+      <TextBox className="flex w-auto justify-center align-middle md:px-4  ">
+        <Image
+          src="/HonorByVarna.jpg"
+          width={567.75}
+          height={834}
+          alt={'certificate from Varna municipality'}
+          priority={true}
+        />
       </TextBox>
     </section>
   );
