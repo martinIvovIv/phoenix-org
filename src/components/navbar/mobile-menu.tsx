0ab2a8312c4e5701f8f7ddec3a4ea9f6c80af2d7
martinIvovIv <martinivoviv@gmail.com>
2024-06-19 21:35:55 +0200

    feat(add-nav-icons): adds mobile nav icons and improves styling

--- src/components/navbar/mobile-menu.tsx
+++ src/components/navbar/mobile-menu.tsx
@@ -41,113 +41,164 @@ export const MobileMenu = ({ className }: { className?: string }) => {
         <DropdownMenu.Portal>
           <DropdownMenu.Content
             hideWhenDetached={true}
-            className="bg-primary dark:bg-secondary text-primary-foreground dark:text-primary data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-20 data-[state=open]:fade-in-20 data-[state=closed]:zoom-out-90 data-[state=open]:zoom-in-90 mt-3 flex min-w-[100vw] flex-col items-stretch rounded-l-md rounded-br-sm text-xl font-medium leading-tight tracking-wide"
+            className="bg-primary dark:bg-secondary text-primary-foreground dark:text-primary data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-20 data-[state=open]:fade-in-20 data-[state=closed]:zoom-out-90 data-[state=open]:zoom-in-90 mt-3 flex min-w-[100vw] flex-col items-stretch rounded-b-lg text-xl font-medium leading-tight tracking-wide"
           >
             <Link
               href={HOME_PATH}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === HOME_PATH &&
                   'font-bold underline underline-offset-2'
               )}
             >
-              <DropdownMenu.Item>{m.og_nav_beginning_home()}</DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.home className="size-4" aria-hidden={true} />
+                  {m.og_nav_beginning_home()}
+                </p>
+              </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={ABOUT_MISSION}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === ABOUT_MISSION &&
                   'font-bold underline underline-offset-2'
               )}
             >
               <DropdownMenu.Item>
-                {m.og_nav_about_us()}: {m.og_nav_missions()}
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.userPlus className="size-4" aria-hidden={true} />
+                  {m.og_nav_about_us()}: {m.og_nav_missions()}
+                </p>
               </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={ABOUT_ACTIVITIES}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === ABOUT_ACTIVITIES &&
                   'font-bold underline underline-offset-2'
               )}
             >
               <DropdownMenu.Item>
-                {m.og_nav_about_us()}: {m.og_nav_activities()}
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.userPlus className="size-4" aria-hidden={true} />
+                  {m.og_nav_about_us()}: {m.og_nav_activities()}
+                </p>
               </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={ABOUT_AWARDS}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === ABOUT_AWARDS &&
                   'font-bold underline underline-offset-2'
               )}
             >
               <DropdownMenu.Item>
-                {m.og_nav_about_us()}: {m.og_nav_awards_only()}
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.userPlus className="size-4" aria-hidden={true} />
+                  {m.og_nav_about_us()}: {m.og_nav_awards_only()}
+                </p>
               </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={ABOUT_PROJECTS}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === ABOUT_PROJECTS &&
                   'font-bold underline underline-offset-2'
               )}
             >
               <DropdownMenu.Item>
-                {m.og_nav_about_us()}: {m.og_nav_projects()}
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.userPlus className="size-4" aria-hidden={true} />
+                  {m.og_nav_about_us()}: {m.og_nav_projects()}
+                </p>
               </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={ABOUT_FOUNDERS}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === ABOUT_FOUNDERS &&
                   'font-bold underline underline-offset-2'
               )}
             >
               <DropdownMenu.Item>
-                {m.og_nav_about_us()}: {m.og_nav_founders()}
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.userPlus className="size-4" aria-hidden={true} />
+                  {m.og_nav_about_us()}: {m.og_nav_founders()}
+                </p>
               </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={GALLERY}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === GALLERY && 'font-bold underline underline-offset-2'
               )}
             >
-              <DropdownMenu.Item>{m.og_nav_gallery()}</DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.images className="size-4" aria-hidden={true} />
+                  {m.og_nav_gallery()}
+                </p>
+              </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={JUNIOR}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === JUNIOR && 'font-bold underline underline-offset-2'
               )}
             >
-              <DropdownMenu.Item>{m.og_nav_fenix_junior()}</DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.person className="size-4" aria-hidden={true} />
+                  {m.og_nav_fenix_junior()}
+                </p>
+              </DropdownMenu.Item>
             </Link>
+
             <Separator />
+
             <Link
               href={CONTACTS}
               className={cn(
-                'py-2 text-center',
+                'py-3',
                 pathname === CONTACTS &&
                   'font-bold underline underline-offset-2'
               )}
             >
-              <DropdownMenu.Item>{m.og_nav_contact_us()}</DropdownMenu.Item>
+              <DropdownMenu.Item>
+                <p className="flex flex-row items-center justify-center gap-3">
+                  <Icons.phone className="size-4" aria-hidden={true} />
+                  {m.og_nav_contact_us()}
+                </p>
+              </DropdownMenu.Item>
             </Link>
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
